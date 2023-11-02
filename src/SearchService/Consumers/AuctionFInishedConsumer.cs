3df4e58b56b47b14d6b7c7ee06440870221edadd
ctoxy <cedric.bouve@gmail.com>
2023-11-02 17:17:32 +0100

    end of gateway before dockerizing s6

---
+++ src/SearchService/Consumers/AuctionFInishedConsumer.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService.Consumers
+{
+    public class AuctionFInishedConsumer : IConsumer<AuctionFinished>
+    {
+        public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = (int)context.Message.Amount;
+        }
+
+        auction.Status = "Finished";
+
+        await auction.SaveAsync();
+    }
+    }
+}
